ff0b45d9a427d8577a639710561d65374e1de97b
Peter Meilstrup <peter.meilstrup@gmail.com>
2014-09-08 16:38:02 -0700

    Section 2.4 (none of code actually tested though)

--- chap2.4.rkt
+++ chap2.4.rkt
@@ -1,8 +1,12 @@
 #lang planet neil/sicp
 
+;;; This is the first problem set that asks for you to assume the
+;;; presence of significant components (rather than building them up.)
+;;; So you can't actually run the code for these solutions....
+
 ;;; Exercise 2.73
 
-;; A. For operators, we look up teh operator in a table and call its
+;; A. For operators, we look up the operator in a table and call its
 ;; generic 'deriv method. There are an inifnite number of distinct
 ;; values for which (number?) or (variable?) is true, so numbers and
 ;; variables cannot be looked up in a table.
@@ -53,11 +57,80 @@
   (put 'deriv '(**) make-sum))
 
 ;; D.
-; This is just transposing the tables -- the arguments to 'put would
-; have to be transposed to correspond.
+;;
+;; This is just transposing the tables -- the arguments to "put" would
+;; have to be transposed to correspond. Alternately, this is
+;; message-passing as described below, but with tables instead of
+;; closures.
+
+;;; Exercise 2.74
+;;
+;; (not runnable because Set and get and set primitives not there
+;; yet. "Suppose that" means I'm not being tasked with implementing
+;; the complete thing.)
+
+;; For each department, we will have written something like this:
+(define (install-accounts-payable-department)
+  (define (get-record data name)
+    (let (record (get data name))
+      (if (null? data) nil)
+      (attach-tag 'accounts-payable-record rec)))
+  (define (get-salary record)
+    ;; do stuff to get the salary
+    'salary)
+  (put 'get-record '(accounts-payable-record) get-record)
+  (put 'get-salary '(accounts-payable-department) get-salary)
+  'done)
+
+;; A.
+(define (get-record file name)
+  (get (type-tag file) 'get-record) (contents file) name)
+;; In this case the "file" should be type-tagged with its
+;; <department> name.  The return value will be type-tagged with
+;; <department>-record.
+;; Methods for get-record and get-salary need to be made for each
+;; <department>-file type.
+
+;; B.
+(define (get-salary record)
+  ((get (type-tag record) 'get-salary) (contents record)))
+;; As stated above the record will be type-tagged with <department>-record,
+;; and there will be get-salary methods for each record-type.
+
+;; C.
+(define (find-employee-record files name)
+  (if (null? files) null
+      (let ((rec (get-record (car files) name)))
+        (if (null? rec) (find-employee-record (cdr files) name)
+            (get-salary rec)))))
 
 
-;;; pasted from book or earlier HWs
+;; D.
+;; Interfacing to a new department requires interfacing methods be
+;; written for get-record and get-salary.
+
+;;; Exercise 2.75.
+
+(define (make-from-mag-ang mag ang)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* mag (cos ang)))
+          ((eq? op 'imag-part) (* mag (sin ang)))
+          ((eq? op 'magnitude) mag)
+          ((eq? op 'angle) ang)
+          ((else (error "Unknown op: MAKE-FROM-MAG-ANG" op)))))
+  dispatch)
+
+;;; Exercise 2.76.
+
+;; Message-pasing style facilitates the addition of new types (since
+;; everything you need to write for a new type is in one place.) You
+;; write a closure for each type (or, using tables, write the
+;; "install" gathering all methods for a type together.)
+;; Data-directed style facilitates addition of new operations (since
+;; you can put similar operations for different types together.)
+;; Tables can be used for both, mind.
+
+;;; pasted from book or earlier HWs:
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -97,14 +170,14 @@
 ;;; type tagging
 (define (attach-tag type-tag contents)
      (cons type-tag contents))
-   (define (type-tag datum)
+(define (type-tag datum)
      (if (pair? datum)
-(car datum)
+         (car datum)
          (error "Bad tagged datum: TYPE-TAG" datum)))
-   (define (contents datum)
-     (if (pair? datum)
-         (cdr datum)
-         (error "Bad tagged datum: CONTENTS" datum)))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
 
 ;;; constructors
 (define (real-part z) (apply-generic 'real-part z))
