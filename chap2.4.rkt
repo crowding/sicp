be3b577c8efa7a67d0f88474294e9f82288b7f26
Peter Meilstrup <peter.meilstrup@gmail.com>
2014-09-12 01:21:12 -0700

    through 2.80

--- chap2.4.rkt
+++ chap2.4.rkt
@@ -101,8 +101,7 @@
   (define (my-deriv basexp var)
     (let ((base (car basexp))
           (exp (cadr basexp)))
-      (make-product
-       (make-product exp (make-exp base (-1 exp))) ;nb. var in exponent not supported
+      (make-product exp (make-exp base (-1 exp)) ;nb. var in exponent not supported
        (deriv (base exp) var))))
   (put 'deriv '(**) my-deriv))
 
@@ -204,28 +203,52 @@
 
 ;;; Exercise 2.75.
 
-(let ()
-  (define (make-from-mag-ang mag ang)
-    (define (dispatch op)
-      (cond ((eq? op 'real-part) (* mag (cos ang)))
-            ((eq? op 'imag-part) (* mag (sin ang)))
-            ((eq? op 'magnitude) mag)
-            ((eq? op 'angle) ang)
-            (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
-    dispatch)
-  'done)
+(define (make-from-mag-ang mag ang)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* mag (cos ang)))
+          ((eq? op 'imag-part) (* mag (sin ang)))
+          ((eq? op 'magnitude) mag)
+          ((eq? op 'angle) ang)
+          (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
+  dispatch)
 
 ;;; Exercise 2.76.
 
-;; Message-pasing style facilitates the addition of new types (since
+;; Message-passing style facilitates the addition of new types (since
 ;; everything you need to write for a new type is in one place.) You
 ;; write a closure for each type (or, using tables, write the
 ;; "install" gathering all methods for a type together.)
+;; Using closures, it also effectively "hides" data.
+;;
 ;; Data-directed style facilitates addition of new operations (since
 ;; you can put similar operations for different types together.)
-;; Tables can be used for both, mind.
+;; Tables can be used for both row-wise and col-wise organization.
 
 ;;; pasted from book or earlier HWs:
+
+;;; type tagging
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (write "type-tags: ")
+    (write type-tags)
+    (write "\n")
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error
+           "No method for these types: APPLY-GENERIC"
+           (list op type-tags))))))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -241,39 +264,6 @@
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 (define (=number? exp num) (and (number? exp) (= exp num)))
 
-(define (square x) (* x x))
-
-(define (add-complex z1 z2)
-  (make-from-real-imag (+ (real-part z1) (real-part z2))
-                       (+ (imag-part z1) (imag-part z2))))
-
-(define (sub-complex z1 z2)
-  (make-from-real-imag (- (real-part z1) (real-part z2))
-                       (- (imag-part z1) (imag-part z2))))
-
-(define (mul-complex z1 z2)
-  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
-                     (+ (angle z1) (angle z2))))
-
-(define (div-complex z1 z2)
-  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
-                     (- (angle z1) (angle z2))))
-
-(define (rectangular? z) (eq? (type-tag z) 'rectangular))
-(define (polar? z) (eq? (type-tag z) 'polar))
-
-;;; type tagging
-(define (attach-tag type-tag contents)
-     (cons type-tag contents))
-(define (type-tag datum)
-     (if (pair? datum)
-         (car datum)
-         (error "Bad tagged datum: TYPE-TAG" datum)))
-(define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum: CONTENTS" datum)))
-
 ;;; constructors
 (define (real-part z) (apply-generic 'real-part z))
 (define (imag-part z) (apply-generic 'imag-part z))
@@ -283,65 +273,3 @@
   ((get 'make-from-real-imag 'rectangular) x y))
 (define (make-from-mag-ang r a)
   ((get 'make-from-mag-ang 'polar) r a))
-
-;;; packaged rectangular complex
-(define (install-rectangular-package)
-  ;; internal procedures
-  (define (real-part z) (car z))
-  (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) (cons x y))
-  (define (magnitude z)
-    (sqrt (+ (square (real-part z))
-             (square (imag-part z)))))
-  (define (angle z)
-    (atan (imag-part z) (real-part z)))
-  (define (make-from-mag-ang r a)
-    (cons (* r (cos a)) (* r (sin a))))
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'rectangular x))
-  (put 'real-part '(rectangular) real-part)
-  (put 'imag-part '(rectangular) imag-part)
-  (put 'magnitude '(rectangular) magnitude)
-  (put 'angle '(rectangular) angle)
-  (put 'make-from-real-imag 'rectangular
-        (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'rectangular
-        (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-;; packaged polar complex
-(define (install-polar-package)
-  ;; internal procedures
-  (define (magnitude z) (car z))
-  (define (angle z) (cdr z))
-  (define (make-from-mag-ang r a) (cons r a))
-  (define (real-part z)
-    (* (magnitude z) (cos (angle z))))
-  (define (imag-part z)
-    (* (magnitude z) (sin (angle z))))
-  (define (make-from-real-imag x y)
-    (cons (sqrt (+ (square x) (square y)))
-          (atan y x)))
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'polar x))
-  (put 'real-part '(polar) real-part)
-  (put 'imag-part '(polar) imag-part)
-  (put 'magnitude '(polar) magnitude)
-  (put 'angle '(polar) angle)
-  (put 'make-from-real-imag 'polar
-        (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'polar
-        (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-    (write "type-tags: ")
-    (write type-tags)
-    (write "\n")
-    (let ((proc (get op type-tags)))
-      (if proc
-          (apply proc (map contents args))
-          (error
-           "No method for these types: APPLY-GENERIC"
-           (list op type-tags))))))
